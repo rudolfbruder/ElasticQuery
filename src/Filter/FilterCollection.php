c50f7b7b20af6c2f7408736f0652c6d3bae9b8c6
Rudolf Bruder <rudolf.bruder@slovar.sk>
2023-07-25 11:31:48 +0200

    Must not added to Filter Collection used for aggregations.

--- src/Filter/FilterCollection.php
+++ src/Filter/FilterCollection.php
@@ -1,50 +1,59 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace Spameri\ElasticQuery\Filter;
 
-
 class FilterCollection implements FilterInterface
 {
+    /**
+     * @var \Spameri\ElasticQuery\Query\MustCollection
+     */
+    private $mustCollection;
+    private $mustNotCollection;
 
-	/**
-	 * @var \Spameri\ElasticQuery\Query\MustCollection
-	 */
-	private $mustCollection;
+    public function __construct(
+        ?\Spameri\ElasticQuery\Query\MustCollection $mustCollection = null,
+        ?\Spameri\ElasticQuery\Query\MustCollection $mustNotCollection = null
+    ) {
+        if (!$mustCollection) {
+            $mustCollection = new \Spameri\ElasticQuery\Query\MustCollection();
+        }
 
+        if (!$mustNotCollection) {
+            $mustNotCollection = new \Spameri\ElasticQuery\Query\MustCollection();
+        }
 
-	public function __construct(
-		?\Spameri\ElasticQuery\Query\MustCollection $mustCollection = NULL
-	)
-	{
-		if ( ! $mustCollection) {
-			$mustCollection = new \Spameri\ElasticQuery\Query\MustCollection();
-		}
+        $this->mustCollection = $mustCollection;
+        $this->mustNotCollection = $mustNotCollection;
+    }
 
-		$this->mustCollection = $mustCollection;
-	}
+    public function must(): \Spameri\ElasticQuery\Query\MustCollection
+    {
+        return $this->mustCollection;
+    }
 
+    public function mustNot(): \Spameri\ElasticQuery\Query\MustCollection
+    {
+        return $this->mustNotCollection;
+    }
 
-	public function must(): \Spameri\ElasticQuery\Query\MustCollection
-	{
-		return $this->mustCollection;
-	}
+    public function key(): string
+    {
+        return '';
+    }
 
+    public function toArray(): array
+    {
+        $array = [];
+        /** @var \Spameri\ElasticQuery\Query\LeafQueryInterface $item */
+        foreach ($this->mustCollection as $item) {
+            $array['bool']['must'][] = $item->toArray();
+        }
+        foreach ($this->mustNotCollection as $item) {
+            $array['bool']['must_not'][] = $item->toArray();
+        }
 
-	public function key(): string
-	{
-		return '';
-	}
-
-
-	public function toArray(): array
-	{
-		$array = [];
-		/** @var \Spameri\ElasticQuery\Query\LeafQueryInterface $item */
-		foreach ($this->mustCollection as $item) {
-			$array['bool']['must'][] = $item->toArray();
-		}
-
-		return $array;
-	}
-
+        return $array;
+    }
 }
